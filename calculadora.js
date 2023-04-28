50ce6c43a30b60e5411ebfa379bc615840c009c9
CarlosCarrilloP <103901668+CarlosCarrilloP@users.noreply.github.com>
2023-04-28 17:25:38 +0200

    Added first Javascript functions
    -Print any button in the screen
    -Delete all in the screen

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,27 @@
 // Obtener elementos del DOM
 const screen = document.querySelector(".screen");
 const clearButton = document.querySelector(".container input[value='C']");
-const operator = document.querySelector("operator");
+const operator = document.querySelectorAll(".operator");
 const equalButton = document.querySelector(".container input[value='=']");
+const btn = document.querySelectorAll('.btn');
 
+//Bucle que recorre la clase .btn detectando cada click
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => {
+      screen.value += btn[i].value;
+    });
+  }
+//Bucle que recorre la clase .operator detectando cada click
+  for (let i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', () => {
+      screen.value += operator[i].value;
+    });
+  }
+//Funcion que borra el .screen
+clearButton.addEventListener('click', () => {
+  screen.value = "";
+});
+//Funcion igual que devuelve el resultado
+equalButton.addEventListener('click', () => {
+  screen.value = eval(screen.value);
+});
