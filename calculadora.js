3d457e61cebcd13697b441855a3ec22653d82fea
CarlosCarrilloP <103901668+CarlosCarrilloP@users.noreply.github.com>
2023-04-28 17:03:27 +0200

    First Commit
    Added CSS

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+// Obtener elementos del DOM
+const screen = document.querySelector(".screen");
+const clearButton = document.querySelector(".container input[value='C']");
+const operator = document.querySelector("operator");
+const equalButton = document.querySelector(".container input[value='=']");
+
