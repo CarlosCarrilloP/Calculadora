155328691e20f1a3585bc908f19bddefc456e0f3
CarlosCarrilloP <103901668+CarlosCarrilloP@users.noreply.github.com>
2023-05-01 18:16:56 +0200

    Added JS Math funcionality
    -Addition
    -Subtraction
    -Multiplication
    -Division

--- calculadora.js
+++ calculadora.js
@@ -3,25 +3,130 @@ const screen = document.querySelector(".screen");
 const clearButton = document.querySelector(".container input[value='C']");
 const operator = document.querySelectorAll(".operator");
 const equalButton = document.querySelector(".container input[value='=']");
-const btn = document.querySelectorAll('.btn');
+const btn = document.querySelectorAll(".btn");
+
+const sumButton = document.querySelector(".container input[value='+']");
+const subtractButton = document.querySelector(".container input[value='-']");
+const multiplyButton = document.querySelector(".container input[value='*']");
+const divideButton = document.querySelector(".container input[value='/']");
+
+sumButton.addEventListener("click", sumar);
+subtractButton.addEventListener("click", restar);
+multiplyButton.addEventListener("click", multiplicar);
+divideButton.addEventListener("click", dividir);
+
 
 //Bucle que recorre la clase .btn detectando cada click
 for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('click', () => {
-      screen.value += btn[i].value;
-    });
-  }
+  btn[i].addEventListener("click", () => {
+    screen.value += btn[i].value;
+  });
+}
 //Bucle que recorre la clase .operator detectando cada click
-  for (let i = 0; i < operator.length; i++) {
-    operator[i].addEventListener('click', () => {
-      screen.value += operator[i].value;
-    });
-  }
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", () => {
+    screen.value += operator[i].value;
+  });
+}
 //Funcion que borra el .screen
-clearButton.addEventListener('click', () => {
+clearButton.addEventListener("click", () => {
   screen.value = "";
 });
+
 //Funcion igual que devuelve el resultado
-equalButton.addEventListener('click', () => {
-  screen.value = eval(screen.value);
+equalButton.addEventListener("click", () => {
+  const valorActual = parseFloat(screen.value);
+  const valoresSuma = screen.value.split("+");
+  const valoresResta = screen.value.split("-");
+  const valoresMultiplicacion = screen.value.split("*");
+  const valoresDivision = screen.value.split("/");
+  let resultado = 0;
+  if (valoresSuma.length > 1) {
+    // Realizamos una suma
+    for (let i = 0; i < valoresSuma.length; i++) {
+      resultado += parseFloat(valoresSuma[i]);
+    }
+  } else if (valoresResta.length > 1) {
+    // Realizamos una resta
+    resultado = parseFloat(valoresResta[0]);
+    for (let i = 1; i < valoresResta.length; i++) {
+      resultado -= parseFloat(valoresResta[i]);
+    }
+  } else if (valoresMultiplicacion.length > 1) {
+    // Realizamos una multiplicación
+    resultado = 1; // Inicializamos en 1 para que la primera multiplicación no sea 0
+    for (let i = 0; i < valoresMultiplicacion.length; i++) {
+      resultado *= parseFloat(valoresMultiplicacion[i]);
+    }
+  } else if (valoresDivision.length > 1) {
+    // Realizamos una división
+    resultado = parseFloat(valoresDivision[0]);
+    for (let i = 1; i < valoresDivision.length; i++) {
+      resultado /= parseFloat(valoresDivision[i]);
+    }
+  } else {
+    // No hay operación a realizar
+    resultado = valorActual;
+  }
+  screen.value = resultado;
 });
+
+
+
+
+
+///////////////Funciones matematicas////////
+//Funcion suma
+function sumar() {
+  const valorActual = parseFloat(screen.value);
+  const valores = screen.value.split("+");
+  let resultado = 0;
+  for (let i = 0; i < valores.length; i++) {
+    resultado += parseFloat(valores[i]);
+  }
+  screen.value = resultado;
+}
+
+
+
+//Funcion resta
+function restar() {
+  const valorActual = parseFloat(screen.value);
+  const valores = screen.value.split("-");
+  let resultado = parseFloat(valores[0]); // Inicializamos el resultado con el primer número
+  for (let i = 1; i < valores.length; i++) { // Empezamos en 1 para evitar el primer número que ya está en el resultado
+    resultado -= parseFloat(valores[i]);
+  }
+  screen.value = resultado;
+}
+
+
+//Funcion multiplicacion
+function multiplicar() {
+  const screen = document.getElementsByClassName('screen'); // Se obtiene el elemento con nombre de la clase 'screen'
+  const valores = screen.value.split('*'); // Se separan los valores de la pantalla por el operador *
+  let resultado = 1;
+
+  for (let i = 0; i < valores.length; i++) {
+    const valorActual = parseFloat(valores[i]); // Se convierte cada valor a número
+    resultado *= valorActual; // Se realiza la multiplicación de cada valor
+  }
+
+  screen.value = resultado; // Se asigna el resultado a la pantalla
+}
+
+
+//Funcion division
+function dividir() {
+  const screen = document.getElementsByClassName('screen'); // Se obtiene el elemento con nombre de clase 'screen'
+  const valores = screen.value.split('/'); // Se separan los valores de la pantalla por el operador /
+  let resultado = parseFloat(valores[0]); // Inicializamos el resultado con el primer número
+
+  for (let i = 1; i < valores.length; i++) { // Empezamos en 1 para evitar el primer número que ya está en el resultado
+    const valorActual = parseFloat(valores[i]); // Se convierte cada valor a número
+    resultado /= valorActual; // Se realiza la división de cada valor
+  }
+
+  screen.value = resultado; // Se asigna el resultado a la pantalla
+}
+
